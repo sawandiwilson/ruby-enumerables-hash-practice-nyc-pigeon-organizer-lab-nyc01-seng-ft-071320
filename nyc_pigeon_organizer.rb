f22cca528698c3a4d6c1365e0eacff305ac16eae
Sawandi Wilson <sawandiwilson@gmail.com>
2020-07-08 16:33:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,13 +6,12 @@ data.each_with_object({}) do |(key, value), final_array|
   #binding.pry
   value.each do |inner_key, names|
     names.each do |name|
-    binding.pry
+    #binding.pry
     if !final_array[name]
       final_array[name] = {}
       end
-      
+      if !final_array[name][key] = []
     end
-    final_array
+    final_array[name][key].push(inner_key)
   end         
-  binding.pry
 end
