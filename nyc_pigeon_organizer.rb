3353ab64b83225eb9512e5fa040dbf4bfb039143
Sawandi Wilson <sawandiwilson@gmail.com>
2020-07-08 16:21:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   # write your code here !
   data.each_with_object({}) do |(key, value), final_array|
   binding.pry
-  final_array|
+  final_array
  a[k] = v + 3
 end         
   binding.pry
