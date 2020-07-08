a69a6fff2386d53dba44b77488cedd590a03c7c2
Sawandi Wilson <sawandiwilson@gmail.com>
2020-07-08 16:21:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,9 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   # write your code here !
-  data.each_with_object({}) do |(k, v), a|
+  data.each_with_object({}) do |(key, value), final_array|
+  binding.pry
+  final_array|
  a[k] = v + 3
 end         
   binding.pry
